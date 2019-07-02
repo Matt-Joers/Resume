9bb0e33d4c6f3f1cb7d6aeb95e865252f8b3e76e
Matt Joers <matthew.joers@froedtert.com>
2019-07-02 10:49:33 -0500

    add folders, contact info, html file

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+contact = {
+  address: 'N391 Lincoln Road',
+  state: "WI",
+  zip: '53036',
+  phone: '262.247.5786',
+  email: 'mattjoers@gmail.com',
+  linkedin: 'https://www.linkedin.com/in/matt-joers-59956b15/',
+  github: 'https://github.com/relicdude36',
+}
